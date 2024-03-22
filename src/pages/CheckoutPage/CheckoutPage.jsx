29f19d47edb96c869dafbead2a3214b631ced8e8
Sowmya Malur <smalur.github@gmail.com>
2024-03-22 01:31:44 -0700

    Update checkoutpage

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,14 +1,35 @@
 import "../CheckoutPage/CheckoutPage.scss";
-
+import close from "../../assets/icons/close.png";
+import rosejam from "../../assets/images/rose_jam_shower_gel_2020_thumbnail_256.png";
 function CheckoutPage() {
+  console.log("I am in checkout page");
   return (
-    <main>
+    <>
       <div className="checkout-page">
-        {/* Your checkout page content */}
-        <h2>Checkout Page</h2>
-        <p>Enter your billing and shipping details.</p>
+        <div className="checkout-page__container">
+          <h2 className="checkout-page__header">Bag (2)</h2>
+          <img src={close} alt="close-icon" className="checkout-page__icon" />
+        </div>
+        <div className="checkout-page__details">
+            <ul className="checkout-page__list">
+                <li className="checkout-page__items">
+                    <div>
+                        <img />
+                        <p>Rose Jam</p>
+                        <p>Shower Gel</p>
+                        <img/>
+                    </div>
+                    <div>
+                        <select className="checkout-page__select"></select>
+                        <button className="checkout-page__button-minus">-</button>
+                        <input></input>
+                        <button className="checkout-page__button-plus">+</button>
+                    </div>
+                    </li>
+            </ul>
+        </div>
       </div>
-    </main>
+    </>
   );
 }
 
