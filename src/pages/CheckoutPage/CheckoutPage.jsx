6ef2d66363e32a89d3c4146e11ca6b1742d38b83
Sowmya Malur <smalur.github@gmail.com>
2024-03-22 03:44:03 -0700

    Add Voucher section

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,13 +1,20 @@
 import "../CheckoutPage/CheckoutPage.scss";
-import close from "../../assets/icons/close.png";
+// TODO: move this to public static backend files
 import rosejam from "../../assets/images/rose_jam_shower_gel_2020_thumbnail_256.png";
+import twilight from "../../assets/images/twilight_body_spray_2020_thumbnail_256.png";
 import logo from "../../assets/logos/commerce-site-logo_22.png";
 import padlock from "../../assets/icons/padlock.png";
+import check from "../../assets/icons/circle.png";
+import voucher from "../../assets/icons/voucher.png";
 
 import { NavLink, Link } from "react-router-dom";
 
 function CheckoutPage() {
-  console.log("I am in checkout page");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+  }
   return (
     <>
       <header className="checkout-header">
@@ -29,33 +36,115 @@ function CheckoutPage() {
       <div className="checkout-page">
         <h2 className="checkout-page__header">Summary & review</h2>
         <div className="checkout-page__details">
+          {/* TODO: get data dynamically? use array.map */}
           <ul className="checkout-page__list">
             <li className="checkout-page__items">
               <div className="outer-container">
-              <div className="checkout-page__col-1">
-                <img
-                  src={rosejam}
-                  alt="rose-jam-shower-gel"
-                  className="checkout-page__image"
-                />
-                <div>
-                  <p className="checkout-page__prod-title">Rose Jam</p>
-                  <p className="checkout-page__prod-type">SHOWER GEL</p>
-                  <div className="checkout-page__row-1">
-                    <p className="checkout-page__vol">250 ml</p>
-                    <p className="checkout-page__qty"> QTY: 1</p>
+                <div className="checkout-page__col-1">
+                  <img
+                    src={rosejam}
+                    alt="rose-jam-shower-gel"
+                    className="checkout-page__image"
+                  />
+                  <div>
+                    <p className="checkout-page__prod-title">Rose Jam</p>
+                    <p className="checkout-page__prod-type">SHOWER GEL</p>
+                    <div className="checkout-page__row-1">
+                      <p className="checkout-page__vol">250 ML</p>
+                      <p className="checkout-page__qty"> QTY: 1</p>
+                    </div>
                   </div>
                 </div>
+                <p className="checkout-page__price">$26.00</p>
+              </div>
+              <div className="checkout-page__row-2">
+                <img
+                  src={check}
+                  alt="instock-icon"
+                  className="checkout-page__icon"
+                />
+                <p className="checkout-page__instock">In stock</p>
+                <p className="checkout-page__name">Lush Cosmetics Center</p>
+              </div>
 
-                {/* <select className="checkout-page__select"></select>
-                <button className="checkout-page__button-minus">-</button>
-                <input></input>
-                <button className="checkout-page__button-plus">+</button> */}
+              <p className="checkout-page__order">
+                Order by 19:00, collect in-store today
+              </p>
+            </li>
+            <li className="checkout-page__items">
+              <div className="outer-container">
+                <div className="checkout-page__col-1">
+                  <img
+                    src={twilight}
+                    alt="twilight-body-spray"
+                    className="checkout-page__image"
+                  />
+                  <div>
+                    <p className="checkout-page__prod-title">Twilight</p>
+                    <p className="checkout-page__prod-type">BODY SPRAY</p>
+                    <div className="checkout-page__row-1">
+                      <p className="checkout-page__vol">200 ML</p>
+                      <p className="checkout-page__qty"> QTY: 1</p>
+                    </div>
+                  </div>
+                </div>
+                <p className="checkout-page__price">$42.00</p>
               </div>
-              <p className="checkout-page__price">$26.00</p>
+              <div className="checkout-page__row-2">
+                <img
+                  src={check}
+                  alt="instock-icon"
+                  className="checkout-page__icon"
+                />
+                <p className="checkout-page__instock">In stock</p>
+                <p className="checkout-page__name">Lush Cosmetics Center</p>
               </div>
+
+              <p className="checkout-page__order">
+                Order by 19:00, collect in-store today
+              </p>
             </li>
           </ul>
+
+          <form id="voucher-form" className="checkout-page__form" onSubmit={handleSubmit}>
+            <div className="checkout-page__voucher">
+              <img
+                src={voucher}
+                alt="voucher-icon"
+                className="checkout-page__icon"
+              />
+              <h2 className="checkout-page__title">Add Voucher</h2>
+            </div>
+            <div className="checkout-page__voucher-info">
+            <input 
+            type="text" 
+            name="voucher" 
+            id="voucher" 
+            className="checkout-page__code"
+            placeholder="Your voucher code"></input>
+            <button className="checkout-page__apply">Apply</button>
+            </div>
+          </form>
+
+
+          <div className="checkout-page__pricing-section">
+            <div className="checkout-page__row-1 checkout-page__row-1--spacing">
+              <p>Subtotal</p>
+              <p>$68.00</p>
+            </div>
+            <div className="checkout-page__row-1 checkout-page__row-1--spacing">
+              <p>Delivery</p>
+              <p>$0.00</p>
+            </div>
+            <div className="checkout-page__row-1 checkout-page__row-1--spacing">
+              <p>Tax</p>
+              <p>$8.16</p>
+            </div>
+            <div className="checkout-page__row-1 checkout-page__row-1--spacing">
+              <p>Total</p>
+              <p>$76.16</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
