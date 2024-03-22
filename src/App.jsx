10c2b1b8a6b1f2cc91e82449fd88ced3d47056f0
Sowmya Malur <smalur.github@gmail.com>
2024-03-21 23:37:13 -0700

    Initialize ip10 client project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+     <p className="app__header">Helvetica replacement</p> 
+     <p className="app__title">Deep Sleep</p>
+    </div>
+  );
+}
+
+export default App;
